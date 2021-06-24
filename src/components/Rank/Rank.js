a2e5e947344fc834298665874275ae70277c4f60
Saif Shariq <40640504+SaifShariq@users.noreply.github.com>
2021-06-24 14:19:14 +0530

    minor fixes

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = () => {
     return (
        <div>
            <div className='white f3'>
-               {'Saif, your current rank is...'}
+               {'Your current rank is...'}
            </div>
            <div className='white f1'>
                {'#5'}
